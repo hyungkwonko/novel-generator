fedf3176d869bc748898f80e00653147d2409c6b
hyungkwonko <sunbloc9@gmail.com>
2018-05-16 21:00:03 +0900

    1st commit

---
+++ model.py
@@ -0,0 +1,67 @@
+
+###############################################
+#### NOVEL GENERATING MODEL
+#### VER 1.0 (2018-5-16)
+#### HANYANG UNIV.
+#### HYUNGKWON KO
+#### hyungkwonko@gmail.com
+###############################################
+
+from __future__ import print_function
+# from keras.models import Sequential
+# from keras.layers import Dense
+# from keras.layers import LSTM
+# from keras.layers import Bidirectional
+# from keras.layers import TimeDistributed
+# from keras.layers import Dropout
+# from keras.models import load_model
+# from keras.utils import plot_model # 모델 시각화
+
+import argparse
+from zprojectstudy.kt_contest.utils import *
+
+ap = argparse.ArgumentParser()
+ap.add_argument('-data_dir', default='C://users//sunbl//desktop//gaechukja.txt')
+ap.add_argument('-batch_size', type=int, default=15) # 배치 사이즈와 seq_length의 차이?
+ap.add_argument('-layer_num', type=int, default=2)
+ap.add_argument('-seq_length', type=int, default=15)
+ap.add_argument('-hidden_dim', type=int, default=256)
+ap.add_argument('-generate_sent_num', type=int, default=15)
+ap.add_argument('-sentvec_dim', type=int, default=300)
+ap.add_argument('-epoch', type=int, default=200)
+# ap.add_argument('-mode', default='train')
+ap.add_argument('-weights', default='')
+# ap.add_argument('-weights', default='C:\\Users\\sunbl\\PycharmProjects\\gthesis\\zprojectstudy\\kt_contest\\novel_layer2_hidden256_epoch10.h5')
+
+args = vars(ap.parse_args())
+
+DATA_DIR = args['data_dir']
+BATCH_SIZE = args['batch_size']
+HIDDEN_DIM = args['hidden_dim']
+SEQ_LENGTH = args['seq_length']
+WEIGHTS = args['weights']
+GENERATE_SENT_NUM = args['generate_sent_num']
+LAYER_NUM = args['layer_num']
+SENTVEC_DIM = args['sentvec_dim']
+
+documents, SENT_SIZE, ix_to_char, char_to_ix = load_data(DATA_DIR)
+sentence_vecs, doc2vec_model = sent_vec(documents, SENTVEC_DIM)
+X, y = load_xy(SENT_SIZE, SEQ_LENGTH, SENTVEC_DIM, sentence_vecs)
+
+# Load trained model if the weight is specified otherwise we can make new model and go through the training step
+if WEIGHTS == '':
+    model = generate_model(HIDDEN_DIM, SEQ_LENGTH, SENTVEC_DIM)
+    epoch = 0
+    while True: # run constantly to improve the performance
+        print('\n\nEpoch: {}\n'.format(epoch))
+        model.fit(X, y, batch_size=10, verbose=1, epochs=1) # nb_epoch is deprecated, use epochs instead
+        epoch += 1
+        # generate_text(model, GENERATE_SENT_NUM, SENT_SIZE, ix_to_char)
+        if epoch % 10 == 0: #  save the weights of  model for every 10 epochs
+            model.save_weights('novel_layer2_hidden{}_epoch{}.h5'.format(HIDDEN_DIM, epoch))
+            generate_text(model, GENERATE_SENT_NUM, SENT_SIZE, SENTVEC_DIM, ix_to_char, doc2vec_model) # show output
+elif WEIGHTS == 'novel_layer2_hidden256_epoch10.h5':
+    trained_model = load_model(HIDDEN_DIM, SEQ_LENGTH, SENTVEC_DIM, WEIGHTS)
+    generate_text(trained_model, GENERATE_SENT_NUM, SENT_SIZE, SENTVEC_DIM, ix_to_char, doc2vec_model)
+else:
+    print('\n\nNothing to do!') # pop up error message
---
+++ utils.py
@@ -0,0 +1,176 @@
+
+###############################################
+#### NOVEL GENERATING MODEL
+#### VER 1.0 (2018-5-16)
+#### HANYANG UNIV.
+#### HYUNGKWON KO
+#### hyungkwonko@gmail.com
+###############################################
+
+# from collections import Counter
+from __future__ import print_function
+import numpy as np
+import re
+from gensim.models import doc2vec
+from konlpy.tag import Kkma
+from collections import namedtuple
+from keras.models import Sequential
+from keras.layers import Dense, LSTM, Bidirectional, TimeDistributed, Dropout
+
+# Function to load the data from local repository and make it as a set of sentences comprised of its component words
+def load_data(data_dir):
+	# data = open("C://users//sunbl//desktop//gaechukja.txt",'r').read()
+	data = open(data_dir, 'r').read() # read out the whole data set
+	# 제거할 것들을 리스트안에 넣고 그걸 translalte 함수로 삭제함
+	havetoerase = ['\n', '?', '!', '-', ':', ',', '(', ')', '<', '>', '金', '明', '天', '掃', '舍', ':'] # this characters will be removed
+	data = data.translate({ord(x): '' for x in havetoerase})
+	documents = re.split('\.|\"', data)
+	while '' in documents: documents.remove('')
+	while '  ' in documents: documents.remove('  ')
+	documents = [i for i in documents if len(i) > 10] # will only care about the sentence containing more than 10 characters.
+	# For example, if the sentence has 8 characters(hi peter.), it will be removed.
+	# I guess this would be helpful making logical connection between sentences since they have a possiblity to cause overfitting problem.
+	SENT_SIZE = len(documents) # numbef of sentences: 215
+	print('Data length: {} characters'.format(len(data))) # number of characters in our data: 7184
+	print('Vocabulary size: {} sentences'.format(SENT_SIZE)) # number of sentences in our data: 215
+	ix_to_char = {ix:documents for ix, documents in enumerate(documents)} # {number : sentence} dictionary # {0: '로', 1: '마', 2: '인', 3: '이', 4: '야', 5: '기'....}
+	char_to_ix = {documents:ix for ix, documents in enumerate(documents)} # the same as above but in reverse order # {'로': 0, '마': 1, '인': 2, '이': 3, '야': 4, '기': 5 ...}
+	return documents, SENT_SIZE, ix_to_char, char_to_ix
+
+# function converting the input data into 3-dimensional data structure for keras model
+def load_xy(sent_size, seq_length, sentvec_dim, sentence_vecs):
+	'''
+	:param sent_size: number of sentences(will be 215 in this case)
+	:param seq_length: sequences I will take into account for each step(will be 15 in this case)
+	:param sentvec_dim: dimension of the sentence vector (will be 300 in this case)
+	:param sentence_vecs: list of sentence vectors
+	'''
+	# make zero-intialized cast like calloc in C
+	X = np.zeros((sent_size-seq_length, seq_length, sentvec_dim)) # reason for subtracting seq_length(15)?
+	y = np.zeros((sent_size-seq_length, seq_length, sentvec_dim))
+	for i in range(sent_size-seq_length): # run 215-15 times. first(0~14), second(1-15), ...last(199~213)
+		for j in range(seq_length):
+			X[i,j] = sentence_vecs[i+j] # X[0] = 0~14th sentences, X[1] = 1~15th, ... X[199] = 199~213rd
+			y[i,j] = sentence_vecs[i+j+1] # y[0] = 1~15th, y[1] = 2~16th, ..., y[199] = 200~214th
+	return X, y # return X and y and they will be the input and target value
+
+# function to tokenize the sentence and merge the word with its PoS(part of speech)
+def tokenize(doc):
+	kkma = Kkma()
+	return ["/".join(t) for t in kkma.pos(doc)] # If we put the sentence list, output it with tagged PoS
+
+# function to make sentence vectors using list of sentences
+def sent_vec(documents, sentvec_dim):
+	train_docs = [tokenize(row) for row in documents]
+	docs = []
+	analyzedDocument = namedtuple('AnalyzedDocument', 'words tags')
+	for i, text in enumerate(train_docs):
+		tags = [i]
+		docs.append(analyzedDocument(text, tags))
+	'''
+	min_count: If the appearance of the word is less than this number, then we will not consider that word
+	window: 
+	worker: 
+	'''
+	model = doc2vec.Doc2Vec(vector_size=sentvec_dim, window=10, min_count=2, workers=4, alpha=0.025, min_alpha=1e-4, seed=777)
+	model.build_vocab(docs)  # Building vocabulary
+	model.train(docs, epochs=1000, total_examples=model.corpus_count)
+	sentence_vecs = np.zeros((len(train_docs), sentvec_dim))
+	for i in range(len(train_docs)):
+		sentence_vecs[i] = model.docvecs[i] # move docvecs to the zero-initialized cast
+	return sentence_vecs, model # return the set of sentenve vectors
+
+# model generating function
+# https://keras.io/layers/recurrent/
+def generate_model(hidden_dim, seq_length, sentvec_dim):
+	'''
+	:param hidden_dim: dimension of each matrix in the cell(have to be power of 2, for example 256, 512. why?)
+	:param seq_length: sequences I will take into account for each step(will be 15 in this case)
+	:param sentvec_dim: dimension of the sentence vector (will be 300 in this case)
+	return_state: whether to return both h_t and c_t. default is false since we generally don't use c_t
+	return _sequence: whether to print in sequence. For example, in case of many to many model, we have to set it TRUE. also for the stacked LSTM layers(obvious)
+	stateful: ??
+	:return: generated model
+	'''
+	model = Sequential()
+	# model.add(Bidirectional(LSTM(hidden_dim, return_sequences=True), input_shape=(seq_length, sentvec_dim), name='BiLSTM_layer')) # be careful what is inside the LSTM bracket and what is not
+	model.add(LSTM(hidden_dim, return_sequences=True, input_shape=(None, sentvec_dim), name='input_LSTM_layer'))
+	model.add(Dropout(0.2))
+	# model.add(LSTM(hidden_dim, return_sequences=True, name='LSTM_layer'))
+	model.add(LSTM(hidden_dim, return_sequences=True, name='LSTM_layer'))
+	model.add(TimeDistributed(Dense(sentvec_dim), name='Dense_layer'))  # 이건 wrapper layer라고 해서 3차원인 우리의 인풋을 2차원으로 바꿔서 output 출력이 가능해주게 변환해줘서 꼭 필요한거라고 하는데 정확히는 모르겟네여..흑흑
+	model.compile(loss="categorical_crossentropy", optimizer="rmsprop")
+	print("This is the architecture of our model!\n")
+	print(model.summary())
+	return model
+
+# if there is trained  model, we can just load it
+def load_model(hidden_dim, seq_length, sentvec_dim, weights):
+	model = Sequential()
+	# model.add(Bidirectional(LSTM(hidden_dim, return_sequences=True), input_shape=(seq_length, sentvec_dim), name='BiLSTM_layer'))
+	model.add(LSTM(hidden_dim, return_sequences=True, input_shape=(None, sentvec_dim), name='input_LSTM_layer'))
+	model.add(Dropout(0.2))
+	model.add(LSTM(hidden_dim, return_sequences=True, name='LSTM_layer'))
+	model.add(TimeDistributed(Dense(sentvec_dim), name='Dense_layer'))
+	model.load_weights(weights, by_name=True)
+	return model
+
+# function to pick an index from a probability array
+def prob_pick(prob, temperature, n_pick):
+	'''
+	:param prob: probabilty array
+	:param temperature:
+	if temperature = 1.0, default value
+	if temperature is big (much bigger than 1), More variety of words will be picked-up from the vocabulary, because the probability gap between words decreases.
+	if temperature is small (close to 0), the highest one will be picked (work like argmax function)
+	:return: the index of the picked-up value (be careful that it is just the index, not the value)
+	'''
+	prob = np.asarray(prob).astype('float64')
+	prob = np.log(prob) / temperature
+	exp_prob = np.exp(prob)
+	prob = exp_prob / np.sum(exp_prob)
+	probability = np.random.multinomial(n_pick, prob, 1)
+	return np.argmax(probability)
+
+# function to generate text
+def generate_text(model, generate_sent_num, sent_size, sentvec_dim, ix_to_char, doc2vec_model):
+	'''
+	:param model: LSTM based model we made above
+	:param generate_length: how many sentences are we gonna generate
+	:param sent_size: number of sentences in the data
+	:param sentvec_dim: dimension of the sentence vector
+	:param ix_to_char: this convert index to sentence
+	:param doc2vec_model: document2vector model we made above
+	:return y_final: THE FINAL OUTCOME(novel we have generated)
+	'''
+	# starting with random character
+	ix = np.random.randint(sent_size) # pick one random value smaller than sent_size (random initial value)
+	# ix = 2 # we can just set it as the third value(0, 1, 2.. so 2 = third)
+	y_final = [ix_to_char[ix]] # return assigned sentence
+	X = np.zeros((1, generate_sent_num, sentvec_dim))
+	for i in range(generate_sent_num):
+		# appending the last predicted character to sequence
+		X[0,i,:] = doc2vec_model.docvecs[ix] # new input x
+		print(ix_to_char[ix], end='.' + "\n") # end는 캐릭터별로 만들거를 붙여쓰려고 예를 들면 a 엔터 p 엔터 p 엔터 l  엔터 e 이렇게 안뽑고 apple 처럼 concatenated form으로 뽑을라고
+		y_vec = model.predict(X[:, :i+1, :])[0,0] # predicted y vector
+		y_candidate = doc2vec_model.docvecs.most_similar([y_vec], topn=7)  # 가장 비슷한 거 3개 뽑는다.
+
+		# 샘플 펑션으로 랜덤하게 픽 하는거 구현해야함
+		# 예를 들면 topn=5개 중에서 3개 뽑는거 이런거..
+		# y_candidate_prob = [y_candidate[0][1], y_candidate[1][1], y_candidate[2][1], y_candidate[4][1], y_candidate[4][1]]
+		# y_candidate_index = [y_candidate[0][0], y_candidate[1][0], y_candidate[2][0], y_candidate[3][0], y_candidate[4][0]]
+		# prob_pick(y_candidate_prob, temperature=0.8, n_pick=3)
+		# # print(y_candidate[0])  # (4, 0.7207441329956055)
+		# print(y_candidate[0][0])  # 4
+		# print(y_candidate[0][1])  # 0.7207441329956055
+		print(y_candidate)
+		print("1st sentence: " + ix_to_char[y_candidate[0][0]])
+		print("2nd sentence: " + ix_to_char[y_candidate[1][0]])
+		print("3rd sentence: " + ix_to_char[y_candidate[2][0]])
+		number = input("please choose one sentece: ")
+		print("\n")
+		ix = y_candidate[int(number)-1][0]
+		y_final.append(ix_to_char[ix]) # 위에서 뽑은 숫자를 다시 캐릭터로 변환하고 그걸 이제 append로 이어 붙임
+	print("\n")
+	print(y_final)
+	return ('. ').join(y_final) # concatenate the output
